9a262c38118272a593fcc0989a323d72cec0e3ce
Pratik Sachdeva <pratik.sachdeva23@gmail.com>
2021-06-02 11:27:18 -0700

    set up init for global importing of package

---
+++ mpl_lego/__init__.py
@@ -0,0 +1,11 @@
+from . import colorbar
+from . import colors
+from . import ellipse
+from . import scatter
+from . import style
+
+__all__ = ["colorbar",
+           "colors",
+           "ellipse",
+           "scatter",
+           "style"]
